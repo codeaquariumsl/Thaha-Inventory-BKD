9a40cf4e1e4f1f2f5f07464f5976ba24660f39fe
sadeepaZincat <sadeepa.zincat@gmail.com>
2026-01-17 01:00:47 +0530

    remove ssync

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const PORT = process.env.PORT || 5001;
 async function startServer() {
     try {
         // Create DB if not exists
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST || 'codeaquariummysql.cpg6i88e4h6e.ap-south-1.rds.amazonaws.com',
-            port: process.env.DB_PORT || 3307,
-            user: process.env.DB_USER || 'code_aqu_inv_user',
-            password: process.env.DB_PASSWORD || 'asdhSFBJ@45gf5'
-        });
-        await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME || 'thaha_inventory'}\`;`);
-        await connection.end();
+        // const connection = await mysql.createConnection({
+        //     host: process.env.DB_HOST || 'codeaquariummysql.cpg6i88e4h6e.ap-south-1.rds.amazonaws.com',
+        //     port: process.env.DB_PORT || 3307,
+        //     user: process.env.DB_USER || 'code_aqu_inv_user',
+        //     password: process.env.DB_PASSWORD || 'asdhSFBJ@45gf5'
+        // });
+        // await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME || 'thaha_inventory'}\`;`);
+        // await connection.end();
 
         // Sync Sequelize
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
 
         // Sync models
-        await sequelize.sync({ alter: true });
+        // await sequelize.sync({ alter: true });
         console.log('Database synced.');
 
         app.listen(PORT, () => {
