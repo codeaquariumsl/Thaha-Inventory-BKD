441e7d083d10e291e7991178fb41d3686d0e77c3
sadeepaZincat <sadeepa.zincat@gmail.com>
2026-01-17 00:53:06 +0530

    edd db port

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER || 'code_aqu_inv_user',
   process.env.DB_PASSWORD || 'asdhSFBJ@45gf5',
   {
-    host: process.env.DB_HOST || 'codeaquariummysql.cpg6i88e4h6e.ap-south-1.rds.amazonaws.com:3307',
+    host: process.env.DB_HOST || 'codeaquariummysql.cpg6i88e4h6e.ap-south-1.rds.amazonaws.com',
+    port: process.env.DB_PORT || 3307,
     dialect: 'mysql',
     logging: false,
     pool: {
